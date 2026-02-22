dcf5d10a2400d75ef64cb3d7c280ad74bcf80fb7
DFun-programming <debmallyapathak2017@gmail.com>
2026-02-22 21:58:53 +0530

    init

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();
 const port = 8000;
+
 app.use(express.json());
+
 app.listen(port,()=>{
     console.log('server is running at http://localhost:8000')
 })
